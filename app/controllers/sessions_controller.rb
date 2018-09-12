de64f078cefd1683706a72a06bb969428e279a6f
Paul O'Shea <poshea48@msn.com>
2018-09-12 09:32:32 -1000

    Added session helpers

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,7 @@ class SessionsController < ApplicationController
   def create
     @user = User.find_by_email(params[:session][:email])
     if @user && @user.authenticate(params[:session][:password])
-      session[:user_id] = @user.id
+      log_in @user 
       flash[:success] = "You have logged in successfully"
       redirect_to tournaments_path
     else
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,13 @@
 module SessionsHelper
+  def log_in(user)
+    session[:user_id] = user.id
+  end
+
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def user_logged_in?
+    !!current_user
+  end
 end
