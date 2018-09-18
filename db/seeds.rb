38e26e865d929f934d7e8d27fedb74437a32d432
Paul O'Shea <poshea48@msn.com>
2018-09-17 15:35:24 -1000

    added seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(first_name: "Paul",
+            last_name: "OShea",
+            email: "poshea48@msn.com",
+            admin: true,
+            password: 'track@@$$this',
+            )
