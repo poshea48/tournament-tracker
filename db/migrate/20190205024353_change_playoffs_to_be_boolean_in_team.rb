54019b18e4c974faeb3ce6674ece10a490c2796c
Paul O'Shea <poshea48@msn.com>
2019-02-20 11:03:47 -1000

    fixed migration for heroku and updated bundler

--- db/migrate/20190205024353_change_playoffs_to_be_boolean_in_team.rb
+++ db/migrate/20190205024353_change_playoffs_to_be_boolean_in_team.rb
@@ -1,5 +1,5 @@
 class ChangePlayoffsToBeBooleanInTeam < ActiveRecord::Migration[5.2]
   def change
-    change_column :teams, :playoffs, :boolean, default: false
+    change_column :teams, :playoffs, 'boolean USING CAST(playoffs AS boolean)', default: false
   end
 end
