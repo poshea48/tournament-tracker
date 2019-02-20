a29f74cdc406aa324ca2ea83993fc52f567f1da3
Paul O'Shea <poshea48@msn.com>
2019-02-20 11:35:55 -1000

    removed remember_token code

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -22,7 +22,8 @@ module SessionsHelper
   def remember(user)
     user.remember
     cookies.permanent.signed[:user_id] = user.id
-    cookies.permanent[:remember_token] = user.remember_token
+    # removed remember_token column from db 
+    # cookies.permanent[:remember_token] = user.remember_token
   end
 
   def forget(user)
